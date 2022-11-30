0001a541951fe993be1e79fd7f1fc22b2ab1a9d9
Konstantin Ananyev <sambyst@gmail.com>
2022-11-30 14:00:06 +0300

    002-data-types: add solution 3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const objKeys = Object.keys(obj);
 
+  const filteredKeys = objKeys.filter((key) => !fields.includes(key));
+
+  const entries = filteredKeys.map(key => [key, obj[key]]);
+
+  return Object.fromEntries(entries);
 };
