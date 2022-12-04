8f04807259eb629b721bc997d0d5e5264fc9d284
Konstantin Ananyev <sambyst@gmail.com>
2022-12-04 12:12:17 +0300

    03: solution for tasks 1/2/3/4

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const targetObjectKeys = path.split('.');
 
+  return (targetObject) => {
+    if (Object.keys(targetObject).length === 0) {
+      return;
+    }
+
+    let currentLevel = targetObject;
+
+    for (const key of targetObjectKeys) {
+      currentLevel = currentLevel[key];
+    }
+
+    return currentLevel;
+  };
 }
