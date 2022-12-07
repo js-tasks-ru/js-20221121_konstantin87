de3df9149c159735867f921d2f99bc63ee538e1e
Konstantin Ananyev <sambyst@gmail.com>
2022-12-07 19:06:30 +0300

    004-oop-basic-intro-to-dom: implement ColumnChart class

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,92 +1,84 @@
 export default class ColumnChart {
-  constructor({data = [], label = '', value, link, formatHeading = (val) => val} = {}) {
+  chartHeight = 50;
+
+  constructor({
+    data = [],
+    label = '',
+    value = 0,
+    link = '',
+    formatHeading = (input) => input
+  } = {}) {
     this.data = data;
     this.label = label;
     this.value = value;
     this.link = link;
     this.formatHeading = formatHeading;
-    this.chartHeight = 50;
     this.render();
   }
 
-  isDataLoading() {
-    return this.data.length === 0;
+  buildChartsTemplate() {
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data.map(item => {
+      const value = String(Math.floor(item * scale));
+      const percent = (item / maxValue * 100).toFixed(0);
+      return (`<div style="--value: ${value}" data-tooltip="${percent}%"></div>`);
+    }).join('');
+
   }
 
-  getColumnProps(data) {
-    const maxValue = Math.max(...data);
-    const scale = 50 / maxValue;
-
-    return data.map(item => {
-      return {
-        percent: (item / maxValue * 100).toFixed(0) + '%',
-        value: String(Math.floor(item * scale))
-      };
-    });
-  }
-
-  getColumnsChartBars() {
-    if (this.data.length > 0) {
-      const calculatedData = this.getColumnProps(this.data);
-      return Object.entries(calculatedData).map(([_, {percent, value}]) => (
-        `<div style="--value: ${value}" data-tooltip="${percent}"></div>`
-      )).join('');
-    }
-  }
-
-  renderCharts() {
+  buildMainTemplate() {
     const formattedHeading = this.formatHeading ? this.formatHeading(this.value) : this.value;
 
-    if (this.isDataLoading()) {
-      return ('<img src="./charts-skeleton.svg" alt="My Happy SVG"/>');
-    }
-
     return (`
-        <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header">${formattedHeading}</div>
-          <div data-element="body" class="column-chart__chart">
-             ${this.getColumnsChartBars()}
-          </div>
-        </div>
-      `);
-  }
-
-  buildTemplate() {
-    const formattedHeading = this.formatHeading ? this.formatHeading(this.value) : this.value;
-    return (`
-      <div class="column-chart ${this.isDataLoading() && 'column-chart_loading'}" style="--chart-height: 50">
-        <div class="column-chart__title column-chart__${this.label}">
+      <div class="column-chart column-chart_loading" style="--chart-height: 50">
+        <div class="column-chart__title">
           Total ${this.label}
-          <a href="/${this.label}" class="column-chart__link">View all</a>
+          <a href="/${this.link}" class="column-chart__link">View all</a>
         </div>
         <div class="column-chart__container">
           <div data-element="header" class="column-chart__header">${formattedHeading}</div>
           <div data-element="body" class="column-chart__chart">
-            ${this.getColumnsChartBars()}
+            ${this.buildChartsTemplate()}
           </div>
         </div>
-      </div>`
-    );
-  }
-
-  update(newData) {
-    this.data = newData;
-    const currentChart = this.element.querySelector('.column-chart__container');
-    const updatedChart = document.createElement('div');
-    updatedChart.innerHTML = this.renderCharts();
-    currentChart.parentNode.replaceChild(updatedChart.firstElementChild, currentChart);
+      </div>
+    `);
   }
 
   render() {
     const wrapper = document.createElement('div');
 
-    wrapper.innerHTML = this.buildTemplate();
+    wrapper.innerHTML = this.buildMainTemplate();
 
     this.element = wrapper.firstElementChild;
+
+    this.toggleLoadingStatus();
+  }
+
+  update(newData) {
+    this.data = newData;
+    this.toggleLoadingStatus();
+    const currentChart = this.element.querySelector('.column-chart__chart');
+    currentChart.innerHTML = this.buildChartsTemplate();
+  }
+
+  toggleLoadingStatus() {
+    if (!this.element) {
+      return;
+    }
+    if (this.data.length) {
+      this.element.classList.remove('column-chart_loading');
+    } else {
+      this.element.classList.add('column-chart_loading');
+    }
   }
 
   remove() {
-    this.element.remove();
+    if (this.element) {
+      this.element.remove();
+    }
   }
 
   destroy() {
